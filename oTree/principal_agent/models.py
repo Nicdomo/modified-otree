7829e089fea4c8c1c1811573915794b118093075
Dominique Dagunton <dedagunton@yahoo.com>
2018-03-26 13:47:36 +0800

    added 0.0 to effort level

--- oTree/principal_agent/models.py
+++ oTree/principal_agent/models.py
@@ -165,7 +165,7 @@ class Group(BaseGroup):
 
 
     agent_work_effort = models.FloatField(
-        choices=[0.1, 0.2, 0.3,0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+        choices=[0.0, 0.1, 0.2, 0.3,0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
         doc="""Agent's work effort, [0, 1]""",
         widget=widgets.Select,
     )
